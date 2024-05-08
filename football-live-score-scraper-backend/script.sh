92a9132d46575277afa1af01c2c66b44949aaa8d
Sahachat Hukhiao <77887797+screamman-ssh@users.noreply.github.com>
2024-05-08 15:48:32 +0700

    Update script.sh

--- football-live-score-scraper-backend/script.sh
+++ football-live-score-scraper-backend/script.sh
@@ -1,2 +1,2 @@
 docker build -t football-ext/scraper-api .
-docker run --name scraper-api --restart always -e NODE_ENV=production -d -p 4231:4231 stormbooster/scraper-api
+docker run --name scraper-api --restart always -e NODE_ENV=production -d -p 4231:4231 football-ext/scraper-api
